975647d8477b47480560dbfafd7d34ddad711224
bagaskaramadhan <bagaskaramadhan@gmail.com>
2021-01-13 00:19:26 +0700

    tester

--- src/components/DataUser.vue
+++ src/components/DataUser.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-    <div>
-      <CreateModal/>
-    </div>
-    <div class="row no-gutters">
-      <div
-        v-for="(users, index) in UsersGetters"
-        :key="index"
-        class="column align-center"
-      >
-        <b-card>
-          <b-card-text>{{ users.name }}</b-card-text>
-          <b-card-text>{{ users.email }}</b-card-text>
-          <b-card-text>{{ users.mobile }}</b-card-text>
-          <b-card-text>{{ users.address }}</b-card-text>
-          <b-button variant="outline-warning" class="mr-5">Edit</b-button>
-          <b-button variant="outline-danger" class="ml-5">Delete</b-button>
+    <div v-if="UsersGetters.length === 0">
+      <div>
+        <b-card class="text-center style-Card">
+          <div class="bg-secondary text-light">
+            There's Currently No Data Exists Please Create by Clicking the
+            <CreateModal />
+          </div>
         </b-card>
       </div>
-      <div v-if="UsersGetters.length === 0">
-        <img
-          src="https://media.giphy.com/media/l0MYICK7vFjhxwxhK/giphy.gif"
-          alt="No-More"
-        />
+    </div>
+    <div v-else>
+      <div>
+        <CreateModal />
+      </div>
+      <div class="row no-gutters">
+        <div
+          v-for="(users, index) in UsersGetters"
+          :key="index"
+          class="column align-center"
+        >
+          <b-card>
+            <b-card-text>{{ users.name }}</b-card-text>
+            <b-card-text>{{ users.email }}</b-card-text>
+            <b-card-text>{{ users.mobile }}</b-card-text>
+            <b-card-text>{{ users.address }}</b-card-text>
+            <b-button variant="outline-warning" class="mr-5">Edit</b-button>
+            <b-button variant="outline-danger" class="ml-5">Delete</b-button>
+          </b-card>
+        </div>
       </div>
     </div>
   </div>
@@ -58,5 +64,8 @@ export default {
 .align-center {
   margin-left: 50px;
   margin-top: 30px;
+}
+.style-Card{
+  margin-top: 20%;
 }
 </style>
